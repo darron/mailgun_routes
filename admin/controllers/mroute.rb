097b2719bbd58aa083e4d493386c2fd1609c8f01
Darron Froese <darron@froese.org>
2014-03-02 20:22:50 -0700

    Clean up the data a bit.

--- admin/controllers/mroute.rb
+++ admin/controllers/mroute.rb
@@ -2,7 +2,13 @@ MailgunRoutes::Admin.controllers :mroute do
 
   get :index  do
     routes = Mroute.all
-    @mroutes = routes.items
+    mroutes = routes.items
+    @mroutes = Hash.new
+    mroutes.each do |mroute|
+      match_this = mroute['expression'].to_s.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)
+      match_action = mroute['actions'].to_s.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)
+      @mroutes[mroute['id']] = { 'id' => mroute['id'] ,'src' => match_this, 'dest' => match_action }
+    end
     render 'mroute/index'
   end
 
