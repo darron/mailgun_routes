2515bec4b40bb3675e375b4f4030fd1be7db34c0
Darron Froese <darron@froese.org>
2014-03-02 20:30:05 -0700

    Listing works.

--- admin/controllers/mroute.rb
+++ admin/controllers/mroute.rb
@@ -7,7 +7,7 @@ MailgunRoutes::Admin.controllers :mroute do
     mroutes.each do |mroute|
       match_this = mroute['expression'].to_s.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)
       match_action = mroute['actions'].to_s.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)
-      @mroutes[mroute['id']] = { 'id' => mroute['id'] ,'src' => match_this, 'dest' => match_action }
+      @mroutes[mroute['id']] = { 'id' => mroute['id'] ,'src' => match_this.to_s.gsub!(/\[|\]|\"/, ''), 'dest' => match_action.to_s.gsub!(/\[|\]|\"/, '') }
     end
     render 'mroute/index'
   end
